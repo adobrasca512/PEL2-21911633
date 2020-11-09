3c0fcd53c263f5c0d2396855283934f0e798070e
Adilem Dobras <59998402+adobrasca512@users.noreply.github.com>
2020-11-09 13:33:42 +0100

    Add files via upload

---
+++ PEL-21911633/cmake-build-debug/CMakeFiles/PEL_21911633.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PEL_21911633.dir/main.cpp.o"
+  "PEL_21911633"
+  "PEL_21911633.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PEL_21911633.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
